f7883041f7df57784efff087ed70ecc302be4e51
Sai K K <saikksub@gmail.com>
2023-08-02 11:10:51 +0530

    update app title

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
+    title: 'QCrow',
     description: 'The best place to start your documentation.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
@@ -34,5 +34,9 @@ export default defineAppConfig({
       exclude: [],
       fluid: true
     }
+  },
+
+  prose: {
+    h1: {}
   }
 })
